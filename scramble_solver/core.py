3a0707ef8e85a269795055c410f31763ac8fb41f
Nikolay Rozanov <nrozanov@iponweb.net>
2021-08-01 20:54:59 +0300

    Changed indentation from tabs to spaces

--- scramble_solver/core.py
+++ scramble_solver/core.py
@@ -10,29 +10,29 @@ RunStats = namedtuple("SolverStats", "RandResult MLResult MLBetter")
 UnsolvableExceptionRe = re.compile('Unsolvable')
 
 def run():
-	parser = argparse.ArgumentParser(description='Compare random and q learning algorithms efficiency while solving sliding puzzle game')
-	parser.add_argument('gsize', type=int, help='game dimension')
-	parser.add_argument('run_num', type=int, help='number of games to play')
-	args = parser.parse_args()
+    parser = argparse.ArgumentParser(description='Compare random and q learning algorithms efficiency while solving sliding puzzle game')
+    parser.add_argument('gsize', type=int, help='game dimension')
+    parser.add_argument('run_num', type=int, help='number of games to play')
+    args = parser.parse_args()
 
-	gsize, run_num = args.gsize, args.run_num
+    gsize, run_num = args.gsize, args.run_num
 
-	actions = ScrambleGame.get_actions()
-	random_solver = RandomSolver(actions, gsize)
-	q_learing_solver = QLearningSolver(actions, gsize)
-	stats = list()
-	for i in range(run_num):
-		try:
-			rand_game, q_learning_game = ScrambleGame(gsize, i), ScrambleGame(gsize, i)
-		except Exception as e:
-			if not UnsolvableExceptionRe.match(str(e)):
-				raise e
-		else:
-			rand_result, q_learning_result = random_solver.solve(rand_game), q_learing_solver.solve(q_learning_game)
-			stat = RunStats(rand_result, q_learning_result, (rand_result - q_learning_result) / q_learning_result * 100)
-			stats.append(stat)
-	print(f'Average value better {sum([stat.MLBetter for stat in stats]) / len(stats)}%')
+    actions = ScrambleGame.get_actions()
+    random_solver = RandomSolver(actions, gsize)
+    q_learing_solver = QLearningSolver(actions, gsize)
+    stats = list()
+    for i in range(run_num):
+        try:
+            rand_game, q_learning_game = ScrambleGame(gsize, i), ScrambleGame(gsize, i)
+        except Exception as e:
+            if not UnsolvableExceptionRe.match(str(e)):
+                raise e
+        else:
+            rand_result, q_learning_result = random_solver.solve(rand_game), q_learing_solver.solve(q_learning_game)
+            stat = RunStats(rand_result, q_learning_result, (rand_result - q_learning_result) / q_learning_result * 100)
+            stats.append(stat)
+    print(f'Average value better {sum([stat.MLBetter for stat in stats]) / len(stats)}%')
 
 
 if __name__ == "__main__":
-	run()
+    run()
--- scramble_solver/random_solver.py
+++ scramble_solver/random_solver.py
@@ -1,19 +1,16 @@
 import random
 
 class RandomSolver:
-	def __init__(self, actions, *args):
-		self.__actions = actions
+    def __init__(self, actions, *args):
+        self.__actions = actions
 
-	def solve(self, game):
-		self.__game = game
-		self.__moves_num = 0
+    def solve(self, game):
+        self.__game = game
+        self.__moves_num = 0
 
-		while True:
-			self.__moves_num += 1
-			action = random.choice(self.__actions)
-			self.__game.step(action)
-			if self.__game.is_solved():
-				return self.__moves_num
-
-
-
+        while True:
+            self.__moves_num += 1
+            action = random.choice(self.__actions)
+            self.__game.step(action)
+            if self.__game.is_solved():
+                return self.__moves_num
